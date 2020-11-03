24a62ce1d9e2369f9d01813da3d35c1e3af97ec8
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-03 11:04:17 -0700

    updated server.js
    changed it to sample code

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-require("dotenv").config();
+const path = require('path');
+const express = require('express');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.status(404);
-  res.send("404: file Not Found");
+  res.send('404: File Not Found');
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
+app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
